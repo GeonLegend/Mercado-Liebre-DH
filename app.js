b94c43019ab1c807196e9e7cfcdbc452b34987c7
GeonLegend <julianleandroded5051@gmail.com>
2023-07-31 09:27:11 -0300

    Micro desafio - Paso I - Clase 17

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.listen(PORT, () => console.log('Escuchando en el puerto ' + PORT));
